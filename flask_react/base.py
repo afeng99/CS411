6ea147fa88cd0f3ed03ea84dadb4e39583e155ae
Erin McAteer <emcateer@bu.edu>
2022-04-07 21:32:51 -0400

    Full Spotify API Call

--- flask_react/base.py
+++ flask_react/base.py
@@ -6,8 +6,8 @@ api = Flask(__name__)
 
 @api.route('/song')
 def my_song():
-    CLIENT_ID = '66d4dc55bde841f68636140e078d7076'
-    CLIENT_SECRET = '73913d377e454e44b90d6e77782da7ed'
+    CLIENT_ID = 'c2e501f4a8ea4d38aa2f76af2216c161'
+    CLIENT_SECRET = 'e5605a38306d47eaa90dd1ef33df0cb6'
 
     AUTH_URL = 'https://accounts.spotify.com/api/token'
 
@@ -22,7 +22,6 @@ def my_song():
     auth_response_data = auth_response.json()
     # save the access token
     access_token = auth_response_data['access_token']
-
     headers = {
         'Authorization': 'Bearer {token}'.format(token=access_token)
     }
@@ -30,8 +29,11 @@ def my_song():
     # base URL of all Spotify API endpoints
     BASE_URL = 'https://api.spotify.com/v1/'
     # Track ID from the URI
-    track_id = '6CDzDgIUqeDY5g8ujExx2f'
+    track_id = '18L6LjRgI7Uzw17NySgJgg'
+    searchQ = 'q=track:enter+artist:lmfao&type=track'
     # actual GET request with proper header
     r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+    #r = requests.get(BASE_URL + 'search?' + searchQ, headers=headers)
     r = r.json()
+    print(r)
     return r
--- flask_react/src/App.js
+++ flask_react/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 function App() {
  const [songData, setSongData] = useState(null)
+ const [value, setValue] = useState(null)
 
   function getData() {
     axios({
@@ -14,7 +15,11 @@ function App() {
     .then((response) => {
       const res =response.data
       setSongData(({
-        danceability: res.danceability}))
+        danceability: res.danceability,
+        energy: res.energy,
+        loudness: res.loudness,
+        acousticness: res.acousticness,
+        instrumentalness: res.instrumentalness}))
     }).catch((error) => {
       if (error.response) {
         console.log(error.response)
@@ -22,12 +27,22 @@ function App() {
         console.log(error.response.headers)
         }
     })}
+
+    function handleChange(event){
+      setValue(({songName: event.target.value}))
+    }
   return (
     <div className="App">
       <header className="App-header">
-       <p>To get song information: </p><button onClick={getData}>Click me</button>
+       <p>To get song information: </p><button onClick={getData()}>Click me</button>
       {songData && <div>
+            <p>Name: Enter Sandman</p>
+            <p>Artist: Metallica</p>
             <p>Danceability: {songData.danceability}</p>
+            <p>Energy: {songData.energy}</p>
+            <p>Loudness: {songData.loudness}</p>
+            <p>Acousticness: {songData.acousticness}</p>
+            <p>Instrumentalness: {songData.instrumentalness}</p>
             </div>
       }
       </header>
