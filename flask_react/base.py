e8385915c0be6be48721a3e064362fc81878061a
kruthikr <kruthikr@bu.edu>
2022-04-10 12:12:36 -0400

    Search By Name/Artist
    the functions and GET requests are all in base.py. we can create separate files as we include more functionality.

--- flask_react/base.py
+++ flask_react/base.py
@@ -4,36 +4,80 @@ from flask import Flask
 
 api = Flask(__name__)
 
+CLIENT_ID = 'OAuth'
+CLIENT_SECRET = 'OAuth'
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+# save the access token
+access_token = auth_response_data['access_token']
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
 @api.route('/song')
-def my_song():
-    CLIENT_ID = 'c2e501f4a8ea4d38aa2f76af2216c161'
-    CLIENT_SECRET = 'e5605a38306d47eaa90dd1ef33df0cb6'
-
-    AUTH_URL = 'https://accounts.spotify.com/api/token'
-
-    # POST
-    auth_response = requests.post(AUTH_URL, {
-        'grant_type': 'client_credentials',
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-    })
-
-    # convert the response to JSON
-    auth_response_data = auth_response.json()
-    # save the access token
-    access_token = auth_response_data['access_token']
-    headers = {
-        'Authorization': 'Bearer {token}'.format(token=access_token)
-    }
-
+def my_song(track_id):
+    # track ID should be from the spotify URI
     # base URL of all Spotify API endpoints
     BASE_URL = 'https://api.spotify.com/v1/'
-    # Track ID from the URI
-    track_id = '18L6LjRgI7Uzw17NySgJgg'
-    searchQ = 'q=track:enter+artist:lmfao&type=track'
     # actual GET request with proper header
     r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
-    #r = requests.get(BASE_URL + 'search?' + searchQ, headers=headers)
     r = r.json()
     print(r)
     return r
+
+@api.route('/search/<songname>/<artistname>')
+def search(songname,artistname):
+    print()
+    SEARCH_URL = 'https://api.spotify.com/v1/search'
+    # create and fill dictionary with parameters for spotify search API GET request
+    # parameters: search for keyword in titles of tracks, top 50 results in US market
+    myparams = {}
+    myparams['type'] = 'track'
+    myparams['limit'] = '50'
+    myparams['market'] = 'US'
+    myparams['q'] =  songname 
+    # actual GET request with proper header
+    resp = requests.get(SEARCH_URL, params=myparams, headers=headers)
+    resp = resp.json()
+    #print(resp)
+    # create/populate an array with the names of top 50 songs with the keyword
+    song_names = []
+    # create/populate an array with the artists for top 50 songs 
+    song_artists = []
+    for x in range(0,len(resp['tracks']['items'])):
+        artistsforsong= []
+        for y in range(0, len(resp['tracks']['items'][x]['artists'])):
+            artistsforsong.append(resp['tracks']['items'][x]['artists'][y]['name'])
+        song_names.append(resp['tracks']['items'][x]['name'])
+        song_artists.append(artistsforsong)
+    # search the array with artists names for the desired artist name, if found then save the index, else return NONE
+    # if the artist is found, use the index to get track's URI, and use that to find song characteristics
+    indexsaver = None
+    for x in range(0,len(song_names)):
+        if (artistname in song_artists[x]):
+            indexsaver = x
+            break
+    #print (song_names)
+    #print (song_artists)
+    if (indexsaver == None):
+        return None
+    else:
+        spot_track_id = (resp['tracks']['items'][indexsaver]['uri'])
+        track_ID = spot_track_id[14:]
+        print (track_ID)
+        r = my_song(track_ID)
+        return r
+
+# test cases
+#my_song()
+#search("Location","Khalid")
