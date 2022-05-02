3242a5b6acb25e504dbe1ba9972e6b8bf5d3c3c1
Xiao Wen <90300616+fajjrauce@users.noreply.github.com>
2022-05-02 00:27:46 -0400

    Update Login.js

--- flask_react/src/components/Login.js
+++ flask_react/src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import "./Login.css";
 
 const CLIENT_ID = '66d4dc55bde841f68636140e078d7076'
@@ -7,21 +7,25 @@ const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
 const RESPONSE_TYPE = "token"
 
 class Login extends React.Component {
-
   render() {
-
     return (
       <div className="Login">
-        <header className="Login-header">
-          
-          <h1>Welcome</h1>   
-          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
-                          to Spotify</a>
-          
-        </header>
+        <body>
+          <header className="Login-header">
+            <section>
+              <h1>To Start Your Journey</h1>
+              <button className="Button">
+                <a
+                  href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+                >
+                  Login to Spotify
+                </a>
+              </button>
+            </section>
+          </header>
+        </body>
       </div>
     );
-
   }
 }
 
